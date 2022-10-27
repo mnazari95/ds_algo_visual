0efe4b89002c7505176ceaeaf8e039b8cdd9a74a
hafiz <hafizn2013@outlook.com>
2022-10-26 23:52:52 -0700

    Initial Commit

---
+++ ds_algo_visual/ds_algo_visualApp.swift
@@ -0,0 +1,17 @@
+//
+//  ds_algo_visualApp.swift
+//  ds_algo_visual
+//
+//  Created by Mohammad Nazari on 2022-10-26.
+//
+
+import SwiftUI
+
+@main
+struct ds_algo_visualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
